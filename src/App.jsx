7cd1f546816258d41be214ce05517c811531aa3c
Satyam Revgade <satyamrevgade2@gmail.com>
2025-06-24 17:25:00 +0530

    added .env file

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import Page from "./Components/Page";
 import "./styles.css";
 
 export default function App() {
+  const isDev = import.meta.env.DEV;
+
   return (
     <div className="bg-container">
+      {/* Only used in development */}
+      {isDev && (
+        <div className="env-message">
+          <h1>{import.meta.env.VITE_SECRET_MESSAGE}</h1>
+        </div>
+      )}
       <Page />
     </div>
   );
